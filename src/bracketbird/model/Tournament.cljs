c50e20cfe25155b703661b801b731a5d486be67f
Jonas Green <jonasgreen12345@gmail.com>
2016-04-23 19:35:10 +0200

    more

--- src/bracketbird/model/Tournament.cljs
+++ src/bracketbird/model/Tournament.cljs
@@ -5,7 +5,17 @@
 (defn create [entity-id]
   {:entity-id entity-id
    :teams     []
-   :stages    []})
+   :stages    []
+   :final-ranking []})
+
+
+(defn reset [t])
+
+(defn layout-matches [t])
+
+(defn rebuild [t]
+  (-> (reset t)
+      (layout-matches)))
 
 ;-------
 ; teams
@@ -18,11 +28,19 @@
   (util/entity (teams t) team-id))
 
 (defn add-team [t team-id]
-  (update t :teams conj (team/create team-id)))
+  (-> (update :teams conj (team/create team-id))
+      (rebuild)))
+
+(defn- update-team [t team-id k value]
+  (update-in t [:teams (util/index-of-entity (teams t) team-id)] assoc k value))
 
 (defn update-team-name [t team-id name]
-  (let [ie (util/index-and-entity (teams t) team-id)]
-    (update-in t [:teams (first ie)] assoc :name name)))
+  (update-team t team-id :name name))
 
-(defn update-team-seeding [t team-id seeding])
-(defn delete-team [t team-id])
+(defn update-team-seeding [t team-id seeding]
+  (-> (update-team t team-id :seeding seeding)
+      (rebuild)))
+
+(defn delete-team [t team-id]
+  (-> (assoc t :teams (util/remove-entity (teams t) team-id))
+      (rebuild)))
--- src/bracketbird/util.cljs
+++ src/bracketbird/util.cljs
@@ -1,6 +1,10 @@
 (ns bracketbird.util)
 
 
+
+(defn remove [v idx]
+  (vec (concat (subvec v 0 idx) (subvec v (inc idx)))))
+
 (defn entity [entities e-id]
   (some (fn [e] (when (= e-id (:entity-id e)) e)) entities))
 
@@ -8,3 +12,9 @@
   "Returns [index-of-entity entity]"
   [entities e-id]
   (first (keep-indexed (fn [i e] (when (= e-id (:entity-id e)) [i e])) entities)))
+
+(defn index-of-entity [entities e-id]
+  (first (index-and-entity entities e-id)))
+
+(defn remove-entity [entities e-id]
+  (remove entities (index-of-entity entities e-id)))
